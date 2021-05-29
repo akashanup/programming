ebf51de65c2989f1cfd06ed4833ac8790fe78993
Akash Anup <akashanup.1993@hotmail.com>
2021-05-30 00:27:15 +0530

    CourseScheduleIII

---
+++ CourseScheduleIII/solution.py
@@ -0,0 +1,14 @@
+import heapq
+
+class Solution:
+    def scheduleCourse(self, courses: List[List[int]]) -> int:
+        courses.sort(key=lambda course: course[1])
+        maxCourses = []
+        heapq.heapify(maxCourses)
+        daysTaken = 0
+        for courseDuration, courseDeadline in courses:
+            heapq.heappush(maxCourses, -courseDuration)
+            daysTaken += courseDuration
+            if daysTaken > courseDeadline:
+                daysTaken += heapq.heappop(maxCourses)
+        return len(maxCourses)
