053f0bdb3e8436de5bf49a59f5f8c6913fd9bdcb
Akash Anup <akashanup.1993@hotmail.com>
2021-06-04 20:37:40 +0530

    DisjointSetOrUnion-FindAlgorithmToDetectCycleInAnUndirectedGraph

---
+++ DisjointSetOrUnion-FindAlgorithmToDetectCycleInAnUndirectedGraph/README.md
@@ -0,0 +1,11 @@
+# Disjoint Set (Or Union-Find) | (Detect Cycle in an Undirected Graph)
+
+A disjoint-set data structure is a data structure that keeps track of a set of elements partitioned into a number of disjoint (non-overlapping) subsets.
+
+The application disjoint-set data structure is to check whether a given graph contains a cycle or not.
+Union-Find Algorithm can be used to check whether an undirected graph contains cycle or not.
+
+A union-find algorithm is an algorithm that performs two useful operations on such a data structure:
+- Find: Determine which subset a particular element is in. This can be used for determining if two elements are in the same subset.
+- Union: Join two subsets into a single subset.
+
---
+++ DisjointSetOrUnion-FindAlgorithmToDetectCycleInAnUndirectedGraph/solution.py
@@ -0,0 +1,43 @@
+class Graph:
+    def __init__(self, vertices):
+        self.vertices = vertices
+        self.graph = {}
+
+    def addEdge(self, v1, v2):
+        if v1 in self.graph:
+            self.graph[v1].append(v2)
+        else:
+            self.graph[v1] = [v2]
+
+    def findParent(self, parent, i):
+        if parent[i] == -1:
+            return i
+        else:
+            return self.findParent(parent, parent[i])
+
+    @staticmethod
+    def union(parent, x, y):
+        parent[x] = y
+
+    def initializeParent(self):
+        return {i: -1 for i in self.vertices}
+
+    def isCyclic(self):
+        parent = self.initializeParent()
+        for i in self.graph:
+            for j in self.graph[i]:
+                x, y = self.findParent(parent, i), self.findParent(parent, j)
+                if x == y:
+                    return True
+                else:
+                    self.union(parent, x, y)
+        return False
+
+
+graph = Graph([1, 2, 3, 4, 5])
+graph.addEdge(1, 2)
+graph.addEdge(1, 3)
+graph.addEdge(3, 4)
+graph.addEdge(3, 5)
+graph.addEdge(4, 5)
+print(graph.isCyclic())
