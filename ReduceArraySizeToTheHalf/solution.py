12bf7bf14f423f9f0a7c2d5ce54b41e5737a8477
Akash Anup <akashanup.1993@hotmail.com>
2021-07-06 14:03:44 +0530

    ReduceArraySizeToTheHalf

--- ReduceArraySizeToTheHalf/solution.py
+++ ReduceArraySizeToTheHalf/solution.py
@@ -1,15 +1,13 @@
 class Solution:
-    def minSetSize(self, arr):
+    def minSetSize(self, arr: List[int]) -> int:
         lookup = {}
         for i in arr:
             if i in lookup:
                 lookup[i] = lookup[i] + 1
             else:
                 lookup[i] = 1
-        # print(lookup)
         lookupValues = lookup.values()
         lookupValues = sorted(lookupValues, reverse=True)
-        # print(lookupValues)
         arrLen = len(arr)
         maxLen = arrLen // 2
         count = 0
@@ -19,10 +17,3 @@ class Solution:
             if arrLen <= maxLen:
                 break
         return count
-
-
-print(Solution().minSetSize([3, 3, 3, 3, 5, 5, 5, 2, 2, 7]))
-print(Solution().minSetSize([7, 7, 7, 7, 7, 7]))
-print(Solution().minSetSize([1, 9]))
-print(Solution().minSetSize([1000, 1000, 3, 7]))
-print(Solution().minSetSize([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
