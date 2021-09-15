e15d0f5ee6276c211af08b447dabcc72916b1c2a
Akash Anup <akashanup.1993@hotmail.com>
2021-09-16 01:07:59 +0530

    FindNumberInRotatedArray

---
+++ FindNumberInRotatedArray/README.md
@@ -0,0 +1,16 @@
+# Find Number in Rotated Array
+
+We rotate an ascending order sorted array at some point unknown to user. So for instance, 3 4 5 6 7 might become 5 6 7 3 4. Modify binary search algorithm to find an element in the rotated array in O(log n) time and O(1) Space complexity
+
+
+### Example 1
+```sh
+Input: nums = [3,4,5,6,7,8,9,10,1,2], num = 1
+Output: 8
+```
+
+### Example 2
+```sh
+Input: nums = [3,4,5,6,7,8,9,10,1,2], num = 5
+Output: 2
+```
---
+++ FindNumberInRotatedArray/solution.py
@@ -0,0 +1,32 @@
+import unittest
+
+
+class Solution:
+    def binarySearch(self, nums, num):
+        start = 0
+        end = len(nums)
+        while start < end:
+            mid = start + ((end - start) // 2)
+            if nums[mid] == num:
+                return mid
+            elif nums[start] <= num < nums[mid]:
+                end = mid - 1
+            else:
+                start = mid + 1
+        return -1
+
+
+class UnitTest(unittest.TestCase):
+    def testbinarySearch1(self):
+        actual = Solution().binarySearch(nums=[3, 4, 5, 6, 7, 8, 9, 10, 1, 2], num=1)
+        expected = 8
+        self.assertEqual(actual, expected)
+
+    def testBinarySearch2(self):
+        actual = Solution().binarySearch(nums=[3, 4, 5, 6, 7, 8, 9, 10, 1, 2], num=5)
+        expected = 2
+        self.assertEqual(actual, expected)
+
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
