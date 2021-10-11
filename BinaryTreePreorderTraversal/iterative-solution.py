96c36640555fb4b1c59f1c9f7c94b26676167d4e
Akash Anup <akashanup.1993@hotmail.com>
2021-10-11 17:45:45 +0530

    BinaryTreePreorderTraversal

---
+++ BinaryTreePreorderTraversal/iterative-solution.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        preorder = []
+        stack = [root]
+        while stack:
+            root = stack.pop()
+            if root:
+                preorder.append(root.val)
+                stack.append(root.right)
+                stack.append(root.left)
+        return preorder
