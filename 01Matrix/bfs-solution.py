4824bf0d6c9cbca33abb86b6e5bc53fd43959516
Akash Anup <akashanup.1993@hotmail.com>
2021-10-29 01:04:39 +0530

    01Matrix

--- 01Matrix/bfs-solution.py
+++ 01Matrix/bfs-solution.py
@@ -5,7 +5,7 @@ class Solution:
 
         """
             Since we don't have a straight forward base case to find the nearest zero for any cell, so to solve this type of questions, reverse engineering is required.
-            The idea is to first keep a track of all the elements having zeros in a queue and then use BFS to update the value of nearest zero for its neighbours.
+            The idea is to first keep a track of all the cells having zero in a queue and then use BFS to update the value of nearest zero for its neighbours.
             Since BFS is used, so once a value for any element is found, the values for its neighbours would also be found.
         """
 
@@ -14,13 +14,12 @@ class Solution:
         for row in range(m):
             for col in range(n):
                 if mat[row][col] == 0:
-                    queue.append((row,col))
+                    queue.append((row, col))
                     visited[row][col] = True
-
 
         while queue:
             row, col = queue.pop(0)
-            for r, c in [[row+1, col], [row-1, col], [row, col+1], [row, col-1]]:
+            for r, c in [[row + 1, col], [row - 1, col], [row, col + 1], [row, col - 1]]:
                 if 0 <= r < m and 0 <= c < n and not visited[r][c]:
                     visited[r][c] = True
                     mat[r][c] = 1 + mat[row][col]
--- 01Matrix/solution.py
+++ 01Matrix/solution.py
@@ -2,7 +2,14 @@ class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
         m = len(mat)
         n = len(mat[0])
-        lookup = [[m*n for _ in range(n)] for _ in range(m)]
+        lookup = [[m+n for _ in range(n)] for _ in range(m)]
+        """
+            Since it is very hard to get to the base condition mat[row][col] == 0 every time so, lets go with worst scenario for every cell.
+            1. Assign m+n to be the length for each cell to reach to the nearest zero.
+            2. Traverse the matrix two times from top/left to bottom/right so that in the first traversal, previous rows/columns can be taken as base condition and in second traversal next rows/columns can be taken as base condition.
+            In the first traversal, Start from the first cell and check for the nearest zero either in upward of backward direction.
+            In the second traversal, Start from the last cell and check for the nearest zero either in downward of forward direction.
+        """
         # Either moving up or left
         for row in range(m):
             for col in range(n):
