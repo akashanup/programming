7ea6bccab86c5bce09122dfd27c2dd2420f33fec
Akash Anup <akashanup.1993@hotmail.com>
2021-11-02 00:23:01 +0530

    FindAllPermutationsOfList

---
+++ FindAllPermutationsOfList/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def getAllPermutations(self, nums):
+        if len(nums) <= 1:
+            return [nums]
+        permutations = []
+        for i in range(len(nums)):
+            current = nums[i]
+            remaining = nums[:i] + nums[i+1:]
+            # Generating all permutations where m is first element
+            for permutation in self.getAllPermutations(remaining):
+                permutations.append([current]+permutation)
+        return permutations
+
+
+print(Solution().getAllPermutations([1, 2, 3]))
