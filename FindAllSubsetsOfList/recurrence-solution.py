a25c945e7adae2d043e7cee76f2bd781d295ffd8
Akash Anup <akashanup.1993@hotmail.com>
2021-11-01 22:07:58 +0530

    FindAllSubsetsOfList

--- FindAllSubsetsOfList/recurrence-solution.py
+++ FindAllSubsetsOfList/recurrence-solution.py
@@ -1,15 +1,15 @@
 class Solution:
-    def recur(self, nums, n, subLists, i, temp=[]):
+    def recur(self, nums, n, subLists, i, temp):
         if i < n:
-            subLists.append(temp.copy())
             temp.append(nums[i])
+            subLists.append(temp.copy())
             self.recur(nums, n, subLists, i+1, temp)
             temp.pop()
             self.recur(nums, n, subLists, i+1, temp)
         return subLists
 
     def getAllSubLists(self, nums):
-        return self.recur(nums, len(nums), [], 0)
+        return self.recur(nums, len(nums), [[]], 0, [])
 
 
 print(Solution().getAllSubLists([1, 2, 3]))
