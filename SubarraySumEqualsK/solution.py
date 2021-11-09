23d5b4089d8ece706e80181da0b38d943601035e
Akash Anup <akashanup.1993@hotmail.com>
2021-11-09 12:58:20 +0530

    SubarraySumEqualsK

---
+++ SubarraySumEqualsK/solution.py
@@ -0,0 +1,23 @@
+"""
+Logic:
+    Iterate over the array and keep a track of sum of all the encountered elements (prefix sum). Let's call it as currentSum. Whenever, we find currentSum == k, we have found a required subarray.
+    While iterating, also keep a track of all the currentSum which is calculated. This is done because at any point if currentSum exceeds k then we can check whether currentSum - k has already been found or not. If it does then we have found more subarrays.
+"""
+
+
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        currentSum = 0
+        subarrays = 0
+        hashmap = {}
+        for num in nums:
+            currentSum += num
+            if currentSum == k:
+                subarrays += 1
+            if currentSum - k in hashmap:
+                subarrays += hashmap[currentSum-k]
+            if currentSum in hashmap:
+                hashmap[currentSum] += 1
+            else:
+                hashmap[currentSum] = 1
+        return subarrays
