6a180f012ccbe6a40c2c52b8447eb495b45a58a9
Akash Anup <akashanup.1993@hotmail.com>
2021-11-09 20:37:31 +0530

    ActivitySelectionProblem

--- ActivitySelectionProblem/solution.py
+++ ActivitySelectionProblem/solution.py
@@ -2,14 +2,11 @@ class Solution:
     def maxActivities(self, activities):
         # Sort activities by finish time.
         activities.sort(key=lambda x: x[1])
-        totalActivityCount = len(activities)
-        # First activity is always chosen.
+        # At least once activity would definitely be performed.
         maxActivitiesCount = 1
-        i = 1
         j = 0
-        for i in range(1, totalActivityCount):
+        for i in range(1, len(activities)):
             if activities[i][0] >= activities[j][1]:
                 maxActivitiesCount += 1
                 j = i
-            i += 1
         return maxActivitiesCount
