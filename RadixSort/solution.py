0a9d24f8f06a2fbb6c69f6f60ef332ae08a557fa
Akash Anup <akashanup.1993@hotmail.com>
2021-11-09 11:25:06 +0530

    RadixSort

--- RadixSort/solution.py
+++ RadixSort/solution.py
@@ -4,25 +4,29 @@ class Solution:
         # print(arr)
         lookup = [0] * 10
         for i in arr:
-            index = i / digitPlace
-            lookup[int(index % 10)] += 1
+            index = i // digitPlace
+            lookup[index % 10] += 1
         # print(lookup)
         for i in range(1, 10):
             lookup[i] += lookup[i - 1]
         # print(lookup)
         sortedArr = [None] * len(arr)
         for i in range(len(arr) - 1, -1, -1):
-            index = arr[i] / digitPlace
-            sortedArr[lookup[int(index % 10)] - 1] = arr[i]
-            lookup[int(index % 10)] -= 1
+            index = arr[i] // digitPlace
+            sortedArr[lookup[index % 10] - 1] = arr[i]
+            lookup[index % 10] -= 1
         # print(sortedArr)
         return sortedArr
 
     def radixSort(self, arr):
         maxArr = max(arr)
         digitPlace = 1
-        while int(maxArr) > 0:
+        while maxArr > 0:
             arr = self.countingSort(arr, digitPlace)
             digitPlace *= 10
-            maxArr /= 10
+            maxArr //= 10
+        arr = [i for i in arr if i < 0] + [i for i in arr if i >= 0]
         return arr
+
+
+print(Solution().radixSort([92, 17, 0, -1, -536, -992, -516, -745, 46, 48, 42]))
