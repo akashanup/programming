5bba0c086ff2c8074df4762998f7db760ecc829b
Akash Anup <akashanup.1993@hotmail.com>
2022-02-10 01:22:04 +0530

    BestTimeToBuyAndSellStockWithTransactionFee

--- BestTimeToBuyAndSellStockWithTransactionFee/solution.py
+++ BestTimeToBuyAndSellStockWithTransactionFee/solution.py
@@ -13,27 +13,22 @@ dp[0][1] = -infinity => since we don't have any stocks on 0th day so we can't bu
 DP state:
 dp[i][0] = max(dp[i-1][0], dp[i-1][1]+prices[i-1])
 Since a is 0 which means the action taken on ith day is either Rest* or Sell. Therefore the max profit would be the max of Rest action(dp[i-1][0]) and Sell action(dp[i-1][1]+prices[i-1]). Note: To sell a stock we must have buyed a stock before.
-dp[i][1] =
-When i == 1: max(dp[i-1][1], -prices[i-1]) Since we can't buy a stock on ith day untill and unless it is sold on i-2th day
-When i > 1: max(dp[i-1][1], dp[i-2][0]-prices[i-1])
-Since a is 1 which means the action taken on ith day is either Rest* or Buy. Therefore the max profit would be the max of Rest action(dp[i-1][1]) and Buy action(dp[i-1][0]-prices[i-1]).
+dp[i][1] = max(dp[i-1][1], dp[i-2][0]-prices[i-1]-fee)
+Since a is 1 which means the action taken on ith day is either Rest* or Buy. Therefore the max profit would be the max of Rest action(dp[i-1][1]) and Buy action(dp[i-1][0]-prices[i-1]-fee) because whenever we buy, we pay the fee also.
 At last, we have to return dp[-1][0] because if we don't hold any stock at the end then only we can have the maximum profit.
 """
 
 
 class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
+    def maxProfit(self, prices: List[int], fee: int) -> int:
         days = len(prices)
         dp = [[None for j in range(2)] for i in range(days+1)]
-        
+
         dp[0][0] = 0
         dp[0][1] = -sys.maxsize
-        
+
         for i in range(1, days+1):
             dp[i][0] = max(dp[i-1][0], dp[i-1][1]+prices[i-1])
-            if i == 1:
-                dp[i][1] = max(dp[i-1][1], -prices[i-1])
-            else:
-                dp[i][1] = max(dp[i-1][1], dp[i-2][0]-prices[i-1])
+            dp[i][1] = max(dp[i-1][1], dp[i-1][0]-prices[i-1]-fee)
         
         return dp[-1][0]
