871c2ea1c3e12eead0b5b317875eb0e4c504206b
Akash Anup <akashanup.1993@hotmail.com>
2022-02-16 23:55:38 +0530

    InterleavingString

--- InterleavingString/solution.py
+++ InterleavingString/solution.py
@@ -1,22 +1,24 @@
 class Solution:
-    def dpFn(self, s1, s2, s, dp):
-        # Return true if the end of all strings is reached
+    def dp(self, s1, s2, s, lookup):
+        # If all the three strings are exhausted then we have found the Interleaving String.
         if not s1 and not s2 and not s:
             return True
-        # Return false if the end of string `s` is reached, but `s1` or `s2` is not empty
-        if not s:
+        # If at any point the sum of lengths of s1 and s2 doesn't equl to the length of s then we can't find Interleaving String.
+        if len(s1) + len(s2) != len(s):
             return False
-        key = (s1, s2, s)
-        if key not in dp:
-            # If string s1 is not empty and its first character matches with the first character of s, recur for the remaining substring
-            a = (len(s1) and s[0] == s1[0]) and self.dpFn(s1[1:], s2, s[1:], dp)
-            # If string s2 is not empty and its first character matches with the first character of s, recur for the remaining substring
-            b = (len(s2) and s[0] == s2[0]) and self.dpFn(s1, s2[1:], s[1:], dp)
-            dp[key] = a or b
-        return dp[key]
 
-    def isInterleave(self, s1, s2, s3):
-        if len(s1) + len(s2) == len(s3):
-            return self.dpFn(s1, s2, s3, {})
-        else:
-            return False
+        key = (s, s1, s2)
+        if key not in lookup:
+            a, b = False, False
+            # If the characters at first index of s1 and s matches then check for the remaining characters.
+            if s1 and s1[0] == s[0]:
+                a = self.dp(s1[1:], s2, s[1:], lookup)
+            # If the characters at first index of s2 and s matches then check for the remaining characters.
+            if s2 and s2[0] == s[0]:
+                b = self.dp(s1, s2[1:], s[1:], lookup)
+            # If either of the above scenarios yeilds True then we have Interleaving String.
+            lookup[key] = a or b
+        return lookup[key]
+
+    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        return self.dp(s1, s2, s3, {})
