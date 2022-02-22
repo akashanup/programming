5aece994f2d792468befc06da7d5819325f92222
Akash Anup <akashanup.1993@hotmail.com>
2022-02-23 00:21:46 +0530

    CountVowelsPermutation

--- CountVowelsPermutation/solution.py
+++ CountVowelsPermutation/solution.py
@@ -1,33 +1,28 @@
 class Solution:
-    '''
+    """
         For n length vowel permutations:-
         1. Loop through the vowels
-        2. Fill the first index with the current vowel
-        3. For the next index, fill the values from the followedBy list of chosen vowel
-        4. Repeat/Recur Step 3 till n > 1
-        5. Repeat Step 1 for the remaining vowels
-    '''
-    def dpFn(self, followedBy, vowel, n, lookup):
-        key = (vowel, n)
+        2. For the first index(i.e, lastChar is None) fill it with the vowels
+        3. For all the subsequent indexes, fill the values from the followedBy list of chosen vowel(i.e, followedBy[lastChar])
+        4. Repeat/Recur Step 3 till n == 0. If n becomes zero then it means that we have made a valid string of n length so return 1.
+    """
+    vowels = ["a", "e", "i", "o", "u"]
+    followedBy = {"a": ["e"], "e": ["a", "i"], "i": ["a", "e", "o", "u"], "o": ["u", "i"], "u": ["a"]}
+
+    def dp(self, n, lastChar, lookup):
+        if n == 0:
+            return 1
+        key = (n, lastChar)
         if key not in lookup:
-            if n > 1:
-                count = 0
-                for vowel in followedBy[vowel]:
-                    count += self.dpFn(followedBy, vowel, n - 1, lookup)
-                lookup[key] = count
-            elif n == 1:
-                lookup[key] = len(followedBy[vowel])
+            permutations = 0
+            if not lastChar:
+                for char in self.vowels:
+                    permutations += self.dp(n-1, char, lookup)
             else:
-                lookup[key] = 0
+                for char in self.followedBy[lastChar]:
+                    permutations += self.dp(n-1, char, lookup)
+            lookup[key] = permutations
         return lookup[key] % ((10 ** 9) + 7)
 
     def countVowelPermutation(self, n: int) -> int:
-        vowels = ["a", "e", "i", "o", "u"]
-        if n == 1:
-            return len(vowels)
-        followedBy = {"a": ["e"], "e": ["a", "i"], "i": ["a", "e", "o", "u"], "o": ["u", "i"], "u": ["a"]}
-        count = 0
-        lookup = {}
-        for vowel in vowels:
-            count += self.dpFn(followedBy, vowel, n - 1, lookup)
-        return count % ((10 ** 9) + 7)
+        return self.dp(n, None, {})
