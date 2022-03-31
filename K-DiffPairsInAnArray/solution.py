639f5a9076246e5c2c5c03960dfe3713361b9fb2
Akash Anup <akashanup.1993@hotmail.com>
2022-03-31 15:25:34 +0530

    K-DiffPairsInAnArray

--- K-DiffPairsInAnArray/solution.py
+++ K-DiffPairsInAnArray/solution.py
@@ -1,27 +1,11 @@
 class Solution:
-    def addPair(self, hashmap, pairs, num, counterPart):
-        if counterPart in hashmap and (counterPart != num or (counterPart == num and hashmap[num] > 1)):
-            pairs.add((num, counterPart) if num < counterPart else (counterPart, num))
-        # if counterPart in hashmap:
-        #     if counterPart == num:
-        #         if hashmap[num] > 1:
-        #             pairs.add((num, num))
-        #     else:
-        #         pairs.add((num, counterPart) if num < counterPart else (counterPart, num))
-
-
     def findPairs(self, nums: List[int], k: int) -> int:
-        hashmap = {}
-        for num in nums:
-            if num in hashmap:
-                hashmap[num] += 1
-            else:
-                hashmap[num] = 1
-
+        nums.sort()
         pairs = set()
-        for num in hashmap.keys():
-            # counterPart1 = k - num
-            # counterPart2 = -k - num
-            self.addPair(hashmap, pairs, num,  k + num)
-            self.addPair(hashmap, pairs, num, -k + num)
+        hashset = set()
+        for num in nums:
+            counterPart = num-k
+            if counterPart in hashset:
+                pairs.add(counterPart)
+            hashset.add(num)
         return len(pairs)
