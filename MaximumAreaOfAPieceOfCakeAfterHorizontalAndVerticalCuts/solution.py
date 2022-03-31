ba371ac17d9c6b066271b875afc390d08b5f5672
Akash Anup <akashanup.1993@hotmail.com>
2022-03-31 10:20:04 +0530

    MaximumAreaOfAPieceOfCakeAfterHorizontalAndVerticalCuts

--- MaximumAreaOfAPieceOfCakeAfterHorizontalAndVerticalCuts/solution.py
+++ MaximumAreaOfAPieceOfCakeAfterHorizontalAndVerticalCuts/solution.py
@@ -1,18 +1,23 @@
 class Solution:
     def maxArea(self, h: int, w: int, horizontalCuts: List[int], verticalCuts: List[int]) -> int:
-        horizontalCuts.append(0)
-        horizontalCuts.append(h)
-        verticalCuts.append(0)
-        verticalCuts.append(w)
+        horizontalCuts.sort()
+        verticalCuts.sort()
 
-        horizontalCuts = sorted(horizontalCuts)
-        verticalCuts = sorted(verticalCuts)
+        horizontalCuts = [0] + horizontalCuts + [h]
+        verticalCuts = [0] + verticalCuts + [w]
 
-        maxH = 0
-        maxV = 0
-        for i in range(1, len(horizontalCuts)):
-            maxH = max(maxH, horizontalCuts[i] - horizontalCuts[i-1])
+        """
+            Since we are cutting the pieces in horizontally and vertically.
+            Therefore, the maximum height of any piece would be the maximum difference between two consecutive cuts in horizontalCuts and the maximum width of any piece would be the maximum difference between two consecutive cuts in verticalCuts.
+            Therefore, the maximum area of a piece would be of a piece having maximum height and maximum width.
+        """
 
-        for j in range(1, len(verticalCuts)):
-            maxV = max(maxV, verticalCuts[j] - verticalCuts[j-1])
-        return (maxH * maxV) % (10**9 + 7)
+        maxWidth = 0
+        for x in range(1, len(verticalCuts)):
+            maxWidth = max(maxWidth, verticalCuts[x]-verticalCuts[x-1])
+
+        maxHeight = 0
+        for y in range(1, len(horizontalCuts)):
+            maxHeight = max(maxHeight, horizontalCuts[y]-horizontalCuts[y-1])
+
+        return (maxWidth * maxHeight) % ((10**9)+7)
