cdd89dd6294be14c77a8b410e6b33bf6eb04e579
Akash Anup <akashanup.1993@hotmail.com>
2022-04-14 15:04:41 +0530

    ReverseNodesInK-Group

--- ReverseNodesInK-Group/solution.py
+++ ReverseNodesInK-Group/solution.py
@@ -1,51 +1,38 @@
 # Definition for singly-linked list.
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-
-
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
-    def reverseKGroup(self, head: ListNode, k: int) -> ListNode:
-        reversedLinkedList = None
-        tempLinkedList = None
-        linkedList = head
-        i = 0
-        n = 0
-        while linkedList:
-            n += 1
-            linkedList = linkedList.next
-        n //= k
+    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        size = 0
+        fast = head
+        while fast and fast.next:
+            size += 1
+            fast = fast.next.next
 
-        linkedList = head
-        while True:
-            if i == k:
-                if reversedLinkedList:
-                    tempReversedLinkedList = reversedLinkedList
-                    while tempReversedLinkedList.next:
-                        tempReversedLinkedList = tempReversedLinkedList.next
-                    tempReversedLinkedList.next = tempLinkedList
-                else:
-                    reversedLinkedList = tempLinkedList
-                tempLinkedList = None
-                i = 0
-                n -= 1
-            if n > 0:
-                if tempLinkedList:
-                    listNode = ListNode(linkedList.val)
-                    listNode.next = tempLinkedList
-                    tempLinkedList = listNode
-                else:
-                    tempLinkedList = ListNode(linkedList.val)
-            else:
-                if reversedLinkedList:
-                    tempReversedLinkedList = reversedLinkedList
-                    while tempReversedLinkedList.next:
-                        tempReversedLinkedList = tempReversedLinkedList.next
-                    tempReversedLinkedList.next = linkedList
-                else:
-                    reversedLinkedList = linkedList
-                break
-            i += 1
-            linkedList = linkedList.next
-        return reversedLinkedList
+        if fast:
+            size = size*2 + 1
+        else:
+            size *= 2
+
+        sentinel = ListNode()
+        groupEnd = sentinel
+        node = head
+        for i in range(size//k):
+            temp = groupEnd
+            updateGroupEnd = False
+            for j in range(k):
+                nextNode = temp.next
+                temp.next = node
+                node = node.next
+                temp.next.next = nextNode
+                if not updateGroupEnd:
+                    groupEnd = groupEnd.next
+                    updateGroupEnd = True
+
+        if groupEnd:
+            groupEnd.next = node
+
+        return sentinel.next
+
