927375c6e69ed9ace8f77356b402085bff6e8412
Akash Anup <akashanup.1993@hotmail.com>
2022-05-08 14:30:17 +0530

    CombinationSumIII

---
+++ CombinationSumIII/bfs-solution.py
@@ -0,0 +1,21 @@
+from collections import deque
+
+
+class Solution:
+    def combinationSum3(self, k: int, n: int) -> List[List[int]]:
+        candidates = [_ for _ in range(1, 10)]
+        combinations = []
+        queue = deque([])
+
+        for idx, candidate in enumerate(candidates):
+            queue.append([candidate, idx+1, [candidate]])
+
+        while queue:
+            currSum, idx, combination = queue.popleft()
+            if currSum == n and len(combination) == k:
+                combinations.append(tuple(combination))
+            if len(combination) < k:
+                for i in range(idx, len(candidates)):
+                    if currSum + candidates[i] <= n:
+                        queue.append([currSum + candidates[i], i+1, combination + [candidates[i]]])
+        return set(combinations)
