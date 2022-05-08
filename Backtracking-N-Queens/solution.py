1f5b9a402b4bc7d47421775b06f51f7fbd4082b4
Akash Anup <akashanup.1993@hotmail.com>
2022-05-08 14:00:37 +0530

    Backtracking-N-Queens

--- Backtracking-N-Queens/solution.py
+++ Backtracking-N-Queens/solution.py
@@ -1,59 +1,40 @@
 class Solution:
-    def isValid(self, matrix, n, row, col):
-        for r in range(row):
-            for c in range(n):
-                if matrix[r][c] == 1:
-                    if r == row or c == col:
-                        return False
-                    xf, yf = r, c
-                    while xf < n and yf < n:
-                        if xf == row and yf == col:
-                            return False
-                        xf += 1
-                        yf += 1
-
-                    xb, yb = r, c
-                    while xb >= 0 and yb >= 0:
-                        if xb == row and yb == col:
-                            return False
-                        xb -= 1
-                        yb -= 1
-
-                    xf, yb = r, c
-                    while xf < n and yb >= 0:
-                        if xf == row and yb == col:
-                            return False
-                        xf += 1
-                        yb -= 1
-                    xb, yf = r, c
-                    while xb >= 0 and yf < n:
-                        if xb == row and yf == col:
-                            return False
-                        xb -= 1
-                        yf += 1
+    def isSafe(self, board, n, row, col):
+        # Check for row
+        if 'Q' in board[row]:
+            return False
+        # Check for col
+        for r in range(row+1):
+            if board[r][col] == 'Q':
+                return False
+        # Check for left diagonal
+        r, c = row, col
+        while r >= 0 and c >= 0:
+            if board[r][c] == 'Q':
+                return False
+            r -= 1
+            c -= 1
+        # Check for right diagonal
+        r, c = row, col
+        while r >= 0 and c < n:
+            if board[r][c] == 'Q':
+                return False
+            r -= 1
+            c += 1
         return True
 
-    def assignQueen(self, matrix, n, row, result):
+    def recur(self, board, n, row, ans):
         if row == n:
-            result.append(matrix)
-            return True
-
+            ans.append(["".join([board[i][j] for j in range(n)]) for i in range(n)])
+            return
         for col in range(n):
-            if matrix[row][col] == 0:
-                if self.isValid(matrix, n, row, col):
-                    matrix[row][col] = 1
-                    if self.assignQueen(matrix, n, row+1, result) is True:
-                        return True
-                    matrix[row][col] = 0
-        return False
+            if self.isSafe(board, n, row, col):
+                board[row][col] = 'Q'
+                self.recur(board, n, row+1, ans)
+                board[row][col] = '.'
 
-    def solveNQueens(self, n):
-        matrix = [[0 for _ in range(n)] for _ in range(n)]
-        result = []
-        if self.assignQueen(matrix, n, 0, result):
-            return result[0]
-        else:
-            return []
-
-
-print(Solution().solveNQueens(3))
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        board = [['.' for _ in range(n)] for _ in range(n)]
+        ans = []
+        self.recur(board, n, 0, ans)
+        return ans
