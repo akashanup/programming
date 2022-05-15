da27d0bcb69e58f111e799e8f3aef591309b094a
Akash Anup <akashanup.1993@hotmail.com>
2022-05-15 21:22:38 +0530

    MinimumCostToCutStick

--- MinimumCostToCutStick/solution.py
+++ MinimumCostToCutStick/solution.py
@@ -8,12 +8,12 @@ class Solution:
             minCost = sys.maxsize
             # For a piece of stick, we have to analyze all the cuts to determine the minimum cost to cut.
             for cut in cuts:
-                # If cut can be applied to the piece.
+                # If cut can be applied to the stick.
                 if start < cut < end:
                     # Cost of cut would be => the length of stick + minimum cost of cuts for first piece of stick + minimum cost of cuts for second(last) piece of stick
                     minCost = min(minCost,
                                   end - start + self.dp(start, cut, cuts, lookup) + self.dp(cut, end, cuts, lookup))
-                # If a piece couldn't be cut any further, the cost of cutting would be zero.
+                # If a stick couldn't be cut any further, the cost of cutting would be zero.
             lookup[key] = minCost if minCost != sys.maxsize else 0
         return lookup[key]
 
