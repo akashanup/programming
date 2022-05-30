d7a38de6ff17dfeb2651be8c3cbbeda3cfabfb2d
Akash Anup <akashanup.1993@hotmail.com>
2022-05-30 16:58:37 +0530

    WordLadder

--- WordLadder/solution.py
+++ WordLadder/solution.py
@@ -12,12 +12,12 @@ class Solution:
         """
         if endWord not in wordList:
             return 0
-        
+
         wordList = set(wordList)
         lowercaseChars = string.ascii_lowercase
-        
+
         queue = deque([[beginWord, 1]])
-        
+
         while queue:
             word, level = queue.popleft()
             if word == endWord:
@@ -25,9 +25,9 @@ class Solution:
             for i in range(len(word)):
                 for ch in lowercaseChars:
                     if word[i] != ch:
-                        transformedWord = word[:i] + ch + word[i+1:]
-                        if  transformedWord in wordList:
+                        transformedWord = word[:i] + ch + word[i + 1:]
+                        if transformedWord in wordList:
                             wordList.remove(transformedWord)
-                            queue.append([transformedWord, level+1])
-            
+                            queue.append([transformedWord, level + 1])
+
         return 0
