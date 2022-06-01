65392e1948f349cab42e57dcd3af096c4fdcae70
Akash Anup <akashanup.1993@hotmail.com>
2022-06-01 23:50:32 +0530

    HouseRobberII

--- HouseRobberII/solution.py
+++ HouseRobberII/solution.py
@@ -1,23 +1,31 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        n = len(nums)
-        if n <= 3:
-            return max(nums)
+        houses = len(nums)
+        if houses <= 3:
+            return max(houses)
 
         """
             This question is similar to https://leetcode.com/problems/house-robber.
             The only difference here is that the nums is cyclic.
-            So we just need to return the max of (robbing from 1st house to n-1th house, robbing from 2nd house to nth house).
+            So we just need to return the max of (robbing from 0th house to n-1th house, robbing from 1st house to nth house).
             Rest, the logic remains the same.
         """
 
-        maxAmount = 0
-        for j in range(2):
-            dp = [0] * (n-1)
-            dp[0] = nums[j]
-            dp[1] = max(nums[j], nums[j+1])
-            for i in range(2, n-1):
-                dp[i] = max(nums[(i+j) % n] + dp[i-2], dp[i-1])
+        # Dp for houses[:-1]
+        dp = [None] * (houses-1)
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+        for house in range(2, houses-1):
+            dp[house] = max(nums[house]+dp[house-2], dp[house-1])
 
-            maxAmount = max(maxAmount, dp[-1])
-        return maxAmount
+        maxLoot = dp[-1]
+
+        # Dp for houses[1:]
+        dp = [None] * houses
+        dp[0] = 0
+        dp[1] = nums[1]
+        for house in range(2, houses):
+            dp[house] = max(nums[house]+dp[house-2], dp[house-1])
+
+        return max(maxLoot, dp[-1])
+
