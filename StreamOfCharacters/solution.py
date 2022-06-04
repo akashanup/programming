e3024d812d4ba34fca73e84c37b87b03edb82b76
Akash Anup <akashanup.1993@hotmail.com>
2022-06-05 01:45:12 +0530

    StreamOfCharacters

--- StreamOfCharacters/solution.py
+++ StreamOfCharacters/solution.py
@@ -5,26 +5,51 @@ Logic:
 """
 
 
-class StreamChecker:
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.endOfWord = False
 
+
+class Trie:
+    def __init__(self):
+        self.root = self.getTrieNode()
+
+    def getTrieNode(self):
+        return TrieNode()
+
+    def insert(self, word):
+        root = self.root
+        for ch in word:
+            if ch not in root.children:
+                root.children[ch] = self.getTrieNode()
+            root = root.children[ch]
+        root.endOfWord = True
+
+    def isPrefix(self, word):
+        root = self.root
+        # Reverse the word because we need to search for prefix.
+        for i in range(len(word) - 1, -1, -1):
+            if root.endOfWord:
+                return True
+            if word[i] not in root.children:
+                return False
+            root = root.children[word[i]]
+        return root.endOfWord
+
+
+class StreamChecker:
     def __init__(self, words: List[str]):
-        self.letters = []
-        self.trie = {}
-        for word in set(words):
-            node = self.trie
-            for ch in word[::-1]:
-                if ch not in node:
-                    node[ch] = {}
-                node = node[ch]
-            node['$'] = True
+        self.stream = []
+        self.trie = Trie()
+        for word in words:
+            # Reverse the words so that instead of searching for suffix we need to search for prefix which is comparatively easy.
+            self.trie.insert(word[::-1])
 
     def query(self, letter: str) -> bool:
-        self.letters.append(letter)
-        node = self.trie
-        for i in range(len(self.letters)-1, -1, -1):
-            if '$' in node:
-                return True
-            if self.letters[i] not in node:
-                return False
-            node = node[self.letters[i]]
-        return '$' in node
+        self.stream.append(letter)
+        return self.trie.isPrefix(self.stream)
+
+# Your StreamChecker object will be instantiated and called as such:
+# obj = StreamChecker(words)
+# param_1 = obj.query(letter)
