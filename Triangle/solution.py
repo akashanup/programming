9c081bb542842452270a0c51ebcef29239dd00bf
Akash Anup <akashanup.1993@hotmail.com>
2022-06-13 22:40:23 +0530

    Triangle

---
+++ Triangle/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        for row in range(1, len(triangle)):
+            for col in range(row+1):
+                if col == 0:
+                    triangle[row][col] += triangle[row-1][col]
+                elif col == row:
+                    triangle[row][col] += triangle[row-1][col-1]
+                else:
+                    triangle[row][col] += min(triangle[row-1][col], triangle[row-1][col-1])
+
+        return min(triangle[-1])
