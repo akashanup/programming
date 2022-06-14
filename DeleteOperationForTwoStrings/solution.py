e1767d5a97bd681229691f914956e25b641d615e
Akash Anup <akashanup.1993@hotmail.com>
2022-06-15 00:01:42 +0530

    DeleteOperationForTwoStrings

--- DeleteOperationForTwoStrings/solution.py
+++ DeleteOperationForTwoStrings/solution.py
@@ -1,16 +1,17 @@
 class Solution:
+    def dp(self, w1, w2, i, j, lookup):
+        if (i, j) not in lookup:
+            if i == len(w1) and j == len(w2):
+                lookup[(i, j)] = 0
+            elif i == len(w1):
+                lookup[(i, j)] = len(w2)-j
+            elif j == len(w2):
+                lookup[(i, j)] = len(w1)-i
+            elif w1[i] == w2[j]:
+                lookup[(i, j)] = self.dp(w1, w2, i+1, j+1, lookup)
+            else:
+                lookup[(i, j)] = min(1+self.dp(w1, w2, i, j+1, lookup), 1+self.dp(w1, w2, i+1, j, lookup))
+        return lookup[(i, j)]
+
     def minDistance(self, word1: str, word2: str) -> int:
-        word1Len, word2Len = len(word1), len(word2)
-    
-        # Assume word1 is bigger than word2. If not then swap.
-        if word1Len < word2Len: 
-            word1, word2, word1Len, word2Len = word2, word1, word2Len, word1Len
-
-        #Store solutions of sub problems.(DP approach)
-        last, curr = [0] * (word2Len + 1), [0] * (word2Len + 1)
-
-        for character in word1:
-            for j in range(word2Len):
-                curr[j + 1] = last[j] + 1 if character == word2[j] else max(curr[j], last[j + 1])
-            last, curr = curr, last
-        return word1Len + word2Len - (2 * last[word2Len])
+        return self.dp(word1, word2, 0, 0, {})
