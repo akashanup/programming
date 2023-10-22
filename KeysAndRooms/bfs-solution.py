af5e9d17899e815948a4d6f73bc849b3b925b3f1
Akash Anup <akashanup.1993@hotmail.com>
2023-10-23 00:00:59 +0530

    KeysAndRooms

--- KeysAndRooms/bfs-solution.py
+++ KeysAndRooms/bfs-solution.py
@@ -1,20 +1,12 @@
 class Solution:
     def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
-        numRooms = len(rooms)
-        if numRooms == 0:
-            return  True
-        # If 0th room has no keys then we can't visit any other room.
-        if not rooms[0]:
-            return False
-        keys = set([0])
-        queue = deque([[0, rooms[0]]])
-        roomsVisited = 1
+        visited = set()
+        visited.add(0)
+        queue = deque([0])
         while queue:
-            _, keysFound = queue.popleft()
-            for key in keysFound:
-                if key not in keys:
-                    roomsVisited += 1
-                    keys.add(key)
-                    queue.append([key, rooms[key]])
-        return roomsVisited == numRooms
-            
+            room = queue.popleft()
+            for key in rooms[room]:
+                if key not in visited:
+                    visited.add(key)
+                    queue.append(key)
+        return len(visited) == len(rooms)
