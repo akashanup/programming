0b579db9e803fe25fb9fd0631fabbaaa1638ab52
Akash Anup <akashanup.1993@hotmail.com>
2023-10-25 01:19:13 +0530

    NextGreaterElementI

--- NextGreaterElementI/stack-solution.py
+++ NextGreaterElementI/stack-solution.py
@@ -1,20 +1,18 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
         lookup = {}
-        stack = [sys.maxsize]
-        i = len(nums2)-1
+        stack = []
 
         # Finding next greater element for all elements nums2 and storing in lookup.
-        while i >= 0:
-            while nums2[i] > stack[-1]:
+        for i in range(len(nums2) - 1, -1, -1):
+            while stack and nums2[i] >= stack[-1]:
                 stack.pop()
-            lookup[nums2[i]] = stack[-1]
+            lookup[nums2[i]] = stack[-1] if stack else -1
             stack.append(nums2[i])
-            i -= 1
 
         # Finding the next greater element for all elements of nums1 from lookup
-        result = [-1]*len(nums1)
+        result = [-1] * len(nums1)
         for i, num in enumerate(nums1):
-            result[i] = lookup[num] if lookup[num] != sys.maxsize else -1
+            result[i] = lookup[num]
 
         return result
