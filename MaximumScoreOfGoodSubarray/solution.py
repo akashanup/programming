3fc56f4cc312a09ddbfc3bd9fdfe31c33cce4f45
Akash Anup <akashanup.1993@hotmail.com>
2023-10-28 03:06:33 +0530

    MaximumScoreOfGoodSubarray

--- MaximumScoreOfGoodSubarray/solution.py
+++ MaximumScoreOfGoodSubarray/solution.py
@@ -1,41 +1,38 @@
-import sys
-
-
 class Solution:
-    def nextSmallerElement(self, nums):
-        nextSmaller = [None] * len(nums)
-        stack = [[-sys.maxsize, -1]]
-        for i in range(len(nums) - 1, -1, -1):
-            while nums[i] <= stack[-1][0]:
-                stack.pop()
-            nextSmaller[i] = stack[-1][1]
-            stack.append([nums[i], i])
-        return nextSmaller
-
-    def previousSmallerElement(self, nums):
-        previousSmaller = [None] * len(nums)
-        stack = [[-sys.maxsize, -1]]
+    def getPreviousSmallerItemIndexes(self, nums):
+        stack = []
+        result = [-1] * len(nums)
         for i in range(len(nums)):
-            while nums[i] <= stack[-1][0]:
+            while stack and nums[i] <= nums[stack[-1]]:
                 stack.pop()
-            previousSmaller[i] = stack[-1][1]
-            stack.append([nums[i], i])
-        return previousSmaller
+            if stack:
+                result[i] = stack[-1]
+            stack.append(i)
+        return result
+
+    def getNextSmallerItemIndexes(self, nums):
+        stack = []
+        result = [-1] * len(nums)
+        for i in range(len(nums) - 1, -1, -1):
+            while stack and nums[i] <= nums[stack[-1]]:
+                stack.pop()
+            if stack:
+                result[i] = stack[-1]
+            stack.append(i)
+        return result
 
     def maximumScore(self, nums: List[int], k: int) -> int:
-        nextSmaller = self.nextSmallerElement(nums)
-        previousSmaller = self.previousSmallerElement(nums)
+        previousSmallerItemIndexes = self.getPreviousSmallerItemIndexes(nums)
+        nextSmallerItemIndexes = self.getNextSmallerItemIndexes(nums)
 
-        score = 0
-        for idx, num in enumerate(nums):
-            # previousSmaller[idx] (let's say i) and nextSmaller[idx] (let's say j) ensures that the element present at idx is the minimum in range (i -> j)
-            i = previousSmaller[idx]
-            i += 1
-            j = nextSmaller[idx]
-            if j == -1:
-                j = len(nums)
-            j -= 1
-            if i <= k <= j:
-                score = max(score, num * (j - i + 1))
-
-        return score
+        maxScore = 0
+        for i, num in enumerate(nums):
+            previousSmallerItemIndex = previousSmallerItemIndexes[i]
+            nextSmallerItemIndex = nextSmallerItemIndexes[i]
+            if nextSmallerItemIndex == -1:
+                nextSmallerItemIndex = len(nums)
+            # Now it is clear that num is the minimum in between previousSmallerItemIndex+1 and  nextSmallerItemIndex-1 indexes
+            # So if k lies in between this then have a good subarray
+            if previousSmallerItemIndex + 1 <= k <= nextSmallerItemIndex - 1:
+                maxScore = max(maxScore, ((nextSmallerItemIndex - 1) - (previousSmallerItemIndex + 1) + 1) * num)
+        return maxScore
