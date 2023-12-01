10b634bc7cf775df9e38c048ea92776ea3432874
Akash Anup <akashanup.1993@hotmail.com>
2023-12-01 18:30:03 +0530

    ReductionOperationsToMakeTheArrayElementsEqual

--- ReductionOperationsToMakeTheArrayElementsEqual/solution.py
+++ ReductionOperationsToMakeTheArrayElementsEqual/solution.py
@@ -1,22 +1,12 @@
 class Solution:
     def reductionOperations(self, nums: List[int]) -> int:
-        nums = sorted(nums, reverse=True)
-        if nums[0] == nums[-1]:
-            return 0
-        temp = {}
-        j = 0
-        n = 0
-        while j < len(nums) - 1:
-            n += 1
-            if nums[j] != nums[j+1]:
-                temp[nums[j]] = n
-                n = 0
-            j += 1
-            temp[nums[j]] = n + 1
-        temp.pop(nums[-1])
-        count = 0
-        reductions = 0
-        for i in temp.values():
-            count += i
-            reductions += count
-        return reductions
+        nums.sort()
+        operations = 0
+        # Start reducing the largest number to the second-largest number.
+        for i in range(len(nums) - 2, -1, -1):
+            # Check for only unique values.
+            if nums[i] != nums[i + 1]:
+                # Number of operations required to reduce all the numbers at the right of i to nums[i]
+                operations += len(nums) - (i + 1)
+
+        return operations
