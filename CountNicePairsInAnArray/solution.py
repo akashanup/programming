c400f7eb2e42f253f842d725f10ac6de2a04c403
Akash Anup <akashanup.1993@hotmail.com>
2023-12-02 01:27:54 +0530

    CountNicePairsInAnArray

--- CountNicePairsInAnArray/solution.py
+++ CountNicePairsInAnArray/solution.py
@@ -3,7 +3,7 @@ Logic:
     Given formula => nums[i] + rev(nums[j]) == nums[j] + rev(nums[i])
     Let us try to rearrange the given formula to, nums[i] - rev(nums[i]) == nums[j] + rev(nums[j]).
     After rearranging the formula, the problem is simplified as now we are only interested in nums[i] - rev(nums[i]).
-    So in the given array if we find pairs some pair (i, j) where nums[i] - rev(nums[i]) == nums[j] + rev(nums[j]) then
+    So in the given array if we find some pair (i, j) where nums[i] - rev(nums[i]) == nums[j] + rev(nums[j]) then
     we have our one nice pair.
     Let us try to create a hashmap which stores the key as nums[i] - rev(nums[i]) and its count as value for each i.
     Now for all the values of hashmap which are greater than 1, we can say that we have found some nice pairs.
