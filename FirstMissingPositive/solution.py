013f3b9849e22ab01fc0757af789818c369fe541
Akash Anup <akashanup.1993@hotmail.com>
2023-12-05 23:11:11 +0530

    FirstMissingPositive

--- FirstMissingPositive/solution.py
+++ FirstMissingPositive/solution.py
@@ -1,8 +1,12 @@
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
         i = 0
-        # Cyclic Sort by ignoring numbers which are less then 1 or greater than or equalt to length of array. This is done because cyclic sort sorts an array in o(n) iff numbers are in a proper range.
+        """
+        Cyclic Sort by ignoring numbers which are less then 1 or greater than or equal to length of array. 
+        This is done because cyclic sort sorts an array in o(n) iff numbers are in a proper range.
+        """
         while i < len(nums):
+            # Correct index of nums[i]
             correct = nums[i] - 1
             if nums[i] <= 0 or nums[i] > len(nums) or nums[i] == nums[correct]:
                 i += 1
