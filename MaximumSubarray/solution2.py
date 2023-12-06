879243a508c724b55508e1ac309c45570e65e15d
Akash Anup <akashanup.1993@hotmail.com>
2023-12-06 15:49:28 +0530

    MaximumSubarray

--- MaximumSubarray/solution2.py
+++ MaximumSubarray/solution2.py
@@ -1,13 +1,19 @@
+import sys
+
+
 class Solution:
     def maxSubArray(self, nums) -> int:
         """
             Kadane's algorithm: Kadane's algorithm is an iterative dynamic programming algorithm in which
             we search for a maximum sum contiguous subarray within a one-dimensional numeric array
         """
-        for i in range(1, len(nums)):
-            if nums[i-1] > 0:
-                nums[i] += nums[i-1]
-        return max(nums)
+        maxSum = -sys.maxsize
+        currSum = 0
+        for num in nums:
+            currSum += num
+            maxSum = max(maxSum, currSum)
+            currSum = max(currSum, 0)
+        return maxSum
 
 
 print(Solution().maxSubArray([7, -5, -2, 1, -3, 4, -1, 2, 1, -5, 4]))
