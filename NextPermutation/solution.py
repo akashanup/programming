cd748339f2627c401fca40137e490c2af9f89c67
Akash Anup <akashanup.1993@hotmail.com>
2023-12-08 20:50:58 +0530

    NextPermutation

--- NextPermutation/solution.py
+++ NextPermutation/solution.py
@@ -1,42 +1,40 @@
 class Solution:
-    def nextPermutation(self, nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        """
-            Since we have to find the next greater permutation of array, we can think greedly by modifying 
-            only that part of array which will contribute in determining the next greater permutation.
-            Now, to determine the contributing sub-array, consider the below examples:
-                - [1,2,3,4,5] -(next greater permutation)> [1,2,3,5,4]
-                - [1,2,5,3,4] -(next greater permutation)> [1,2,5,4,3]
-                - [1,2,4,3,5] -(next greater permutation)> [1,2,4,5,3]
-                - [1,2,4,5,3] -(next greater permutation)> [1,2,5,4,3]->[1,2,5,3,4]
-                - [1,3,5,4,2] -(next greater permutation)> [1,4,5,3,2]->[1,4,2,3,5]
-                - [5,4,3,2,1] -(next greater permutation)> [1,2,3,4,5]
-            From the above examples, we can see that if we traverse from end and check for the index(from right) from 
-            where the elements are not in descending order-
-                - If found, it means that this index needs to be updated with the first greater element from right to 
-                get any greater permutation and then to get the NEXT greater permutation we can reverse the array from 
-                further index to end as they were in decreasing order, and by reversing, they will be in increasing 
-                order and that would be our next greater permutation.
-                - Else, Complete array is in descending order so just make it into ascending by reversing it.
-        """
+    # @param A : string
+    # @return an integer
+    def findRank(self, A):
+        firstPermutation = sorted(A)
+        if A == firstPermutation:
+            return 1
+        ans = 2
+        nextPermutationList = self.__nextPermutation(list(firstPermutation))
+        nextPermutation = "".join(nextPermutationList)
+        while A != nextPermutation:
+            ans += 1
+            nextPermutationList = self.__nextPermutation(nextPermutationList)
+            nextPermutation = "".join(nextPermutationList)
+        return ans % 1000003
+
+    def __nextPermutation(self, item):
         # Check for the index from right from where the array doesn't follow the descending order.
-        i = len(nums)-2
-        while i >= 0 and nums[i+1] <= nums[i]:
+        i = len(item) - 2
+        while i >= 0 and item[i + 1] <= item[i]:
             i -= 1
 
         # If any such index is found, then swap its value with the first greater element from the right.
         if i >= 0:
-            j = len(nums)-1
-            while j >= 0 and nums[j] <= nums[i]:
+            j = len(item) - 1
+            while j >= 0 and item[j] <= item[i]:
                 j -= 1
-            nums[i], nums[j] = nums[j], nums[i]
+            item[i], item[j] = item[j], item[i]
 
         # Reverse the further array items
         i += 1
-        j = len(nums)-1
+        j = len(item) - 1
         while i < j:
-            nums[i], nums[j] = nums[j], nums[i]
+            item[i], item[j] = item[j], item[i]
             i += 1
             j -= 1
+        return item
+
+
+print(Solution().findRank("DTNGJPURFHYEW"))
