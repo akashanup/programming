921ec41c16fddd4f46b1ec51f1306ca85c8c2a4f
Akash Anup <akashanup.1993@hotmail.com>
2023-12-09 00:29:56 +0530

    MedianOfTwoSortedArrays

--- MedianOfTwoSortedArrays/solution.py
+++ MedianOfTwoSortedArrays/solution.py
@@ -1,37 +1,43 @@
 """
 Logic:
-    If you have a single sorted array you can easily calculate the median by considering the middle element(s).
-    If we deep dive the above statement we can figure out that the array could be partitioned into two parts where the lengths of two part differs by at max 1.
+    If you have a single sorted array, you can easily calculate the median by considering the middle element(s).
+    If we deep dive the above statement, we can figure out that the array could be partitioned into two parts where the
+    lengths of two parts differ by at max 1.
     For example, Let's say the array is [1,2,3,4,5,6,7,8]
-        Partitioned into two parts: l=>[1,2,3,4]  and  r=>[5,6,7,8]
+        Partitioned into two parts: l=>[1,2,3,4] and r=>[5,6,7,8]
     OR If the array is [1,2,3,4,5,6,7]
-        Partitioned into two parts: l=>[1,2,3,4]  and  r=>[5,6,7]
+        Partitioned into two parts: l=>[1,2,3,4] and r=>[5,6,7]
     Now the median would be,
-        1. If l and r of same length then, median = max(l)+min(r) / 2
+        1. If l and r of the same length then, median = max(l)+min(r) / 2
         2. If l ard r differs by 1 length then, median = max(l)
     Now to calculate the median of two sorted arrays, assume the two arrays are merged and still sorted.
-    Then the length of merged array would be m+n, and it can be partitioned into two at (m+n / 2)th index or the index at half of length of merged array.
-    Now the question arises as how to partition the assumed merged sorted array?
-    Since we know the partition index(half), we can calculate how many values will be on the left part and right part of array.
+    Then the length of merged array would be m+n, and it can be partitioned into two at (m+n / 2)th index or the index
+    at half of the length of merged array.
+    Now the question arises as to how to partition the assumed merged sorted array?
+    Since we know the partition index(half), we can calculate how many values will be on the left part and right part
+    of array.
     For example,
         arr1 => [1,4,5,8,10], m = 5
         arr2 => [2,3,9], n = 3
         Assume the above arrays are merged and sorted, therefore the array would be,
             [1,2,3,4,5,8,9,10], m+n = 8
-        Now the array could be partitioned into two parts of length 4 (4 elements will be in left part and 4 will be in right part).
-    Now the question arises, which of the 4 elements would lie in the left part and which 4 will lie in right.
+        Now the array could be partitioned into two parts of length 4 (4 elements will be in left part and 4 will be in
+        right part).
+    Now the question arises, which of the 4 elements would lie in the left part and which 4 will lie in the right.
     Let's say l1 be the part of arr1 and l2 would be the part of arr2 which would lie on the left part of merged array.
     Similarly, r1 and r2 would lie in the right part of sorted array.
     So, l1+l2 = 4 and r1+r2 = 4
-    Now l1 can be determined by iterating the first array for first 4 elements and for each element, we can check the corresponding values of l2 as l1+l2 == 4.
+    Now l1 can be determined by iterating the first array for the first 4 elements and for each element,
+    we can check the corresponding values of l2 as l1+l2 == 4.
     For example, if,
-        1. l1 => [1,4,5,8] then l2 would be []
-        2. l1 => [1,4,5] then l2 would be [2]
-        3. l1 => [1,4] then l2 would be [2,3]
-        4. l1 => [1] then l2 would be [2,3,9]
-        5. l1 => [] then l2 would be [2,3,9] NOT POSSIBLE because l1+l2 should be equal to 4
+        1. L1 => [1,4,5,8] then l2 would be []
+        2. L1 => [1,4,5] then l2 would be [2]
+        3. L1 => [1,4] then l2 would be [2,3]
+        4. L1 => [1] then l2 would be [2,3,9]
+        5. L1 => [] then l2 would be [2,3,9] NOT POSSIBLE because l1+l2 should be equal to 4
     Similarly, we can calculate r1 and r2.
-    The above is a demonstration of linear search. Since our both arrays are sorted, we could use Binary search for optimal solution.
+    The above is a demonstration of linear search.
+    Since our both arrays are sorted, we could use Binary search for an optimal solution.
     Now the correct combination of l1,l2,r1 and r2 would be such that,
         max(l1) <= min(r2) and max(l2) <= min(r1) i.e,
         l1 => [1,4], r1 => [5,8,10]
@@ -42,6 +48,7 @@ Logic:
 
 
 import sys
+from typing import List
 
 
 class Solution:
@@ -76,3 +83,11 @@ class Solution:
                 else:
                     start = partition1+1
         return 0.0
+
+
+a = [1, 4, 5, 8, 10]
+b = [2, 3, 9]
+c = [1, 2, 3, 4, 5, 8, 9, 10]
+median = (4+5)/2
+print(median)
+print(Solution().findMedianSortedArrays(a, b))
