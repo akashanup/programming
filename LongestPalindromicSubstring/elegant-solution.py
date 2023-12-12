1268518ab1f0f76c0645554e072517f358ece832
Akash Anup <akashanup.1993@hotmail.com>
2023-12-12 10:28:46 +0530

    LongestPalindromicSubstring

--- LongestPalindromicSubstring/elegant-solution.py
+++ LongestPalindromicSubstring/elegant-solution.py
@@ -8,9 +8,11 @@ class Solution:
     def longestPalindrome(self, s: str) -> str:
         longest = ""
         for i in range(len(s)):
-            # Make ith element as the middle element of odd length palindrome string. Keep moving backward and forward as long as the resulting string is palindrome.
+            # Make ith element as the middle element of odd length palindrome string.
+            # Keep moving backward and forward as long as the resulting string is palindrome.
             oddPalindromes = self.checkPalindrome(s, i, i)
-            # Make ith and (i+1)th element as the middle elements of even length palindrome string. Keep moving backward and forward as long as the resulting string is palindrome.
+            # Make ith and (i+1)th element as the middle elements of even length palindrome string.
+            # Keep moving backward and forward as long as the resulting string is palindrome.
             evenPalindromes = self.checkPalindrome(s, i, i + 1)
             if len(oddPalindromes) > len(evenPalindromes):
                 if len(oddPalindromes) > len(longest):
